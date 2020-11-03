c45e65d62ce93262342f6fbfebb626fbf5cda950
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-11-03 18:13:23 +0530

    refactor: code which connect with database along with server

--- server.js
+++ server.js
@@ -4,9 +4,10 @@ const mongoose = require('mongoose');
 const app = express();
 app.use(bodyParser.urlencoded({extended:true}))
 app.use(bodyParser.json());
+require('./config/databaseconfig.js');
 app.get('/', (req, res) => {
     res.json({"message": "Welcome To Greeting Application "});
 });
 app.listen(4000, () => {
-    console.log("Server is listening on port 3000");
+        console.log("Server is listening on port 4000");
 });
