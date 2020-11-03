88f4cf215facfb336c047498225c534da5cb71a8
Saurabh Dagwar <saurabhdagwar2020@gmail.com>
2020-11-04 01:40:28 +0530

    refactor: code to update previous greeting messages according with given greeting id

--- controller/greeting.controller.js
+++ controller/greeting.controller.js
@@ -33,4 +33,39 @@ exports.findAll = (req, res) => {
             message: err.message || "Error Occurred while retriving all Greeting"
         });
     });
-}; 
+};
+
+/*************************************************************************************
+ * @description Following code is used to update data from database
+ * NOTES - Updating Greeting Message from database
+ *************************************************************************************/
+
+exports.update = (req, res) => {
+    if(!req.body.message) {
+        return res.status(400).send({
+            message: "Greeting Message Cannot be Empty"
+        });
+    }
+
+    Greeting.findByIdAndUpdate(req.params.greetingId, {
+        name: req.body.name || "Name Required" ,
+       message: req.body.message 
+    }, {new: true})
+    .then(greeting => {
+        if(!greeting) {
+            return res.status(404).send({
+                message: "Greeting Not found with id" +req.params.greetingId
+            });
+        }
+        res.send(greeting);
+    }).catch(err => {
+        if(err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message:"Greeting Not found with id" +req.params.greetingId
+            });                
+        }
+        return res.status(500).send({
+            message: "Error Updating Greeting with Id"  +req.params.greetingId
+        });
+    });
+};
--- routes/greeting.routes.js
+++ routes/greeting.routes.js
@@ -1,8 +1,9 @@
 /*************************************************************************************
- * Following Program is used to create and recieve data from database
+ * Following Program is used to create, recieve and update data from database
  *************************************************************************************/
 module.exports = (app) => {
     const greeting = require('../controller/greeting.controller.js');
     app.post('/greeting',greeting.create);
     app.get('/greeting',greeting.findAll);
+    app.put('/greeting/:greetingId',greeting.update);
 }
